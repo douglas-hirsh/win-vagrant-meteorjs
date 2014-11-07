a7f9f2274496d258f0f02552bbb7fa5d89ceb25b
Douglas Hirsh <dhirshjr@gmail.com>
2014-11-06 19:14:04 -0600

    initial commit with initial scripts

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty32"
+
+  config.vm.synced_folder './shared', '/home/vagrant/shared', nfs: true
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+
+  config.vm.provider "virtualbox" do |v|
+    v.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root", "1"]
+  end
+end
---
+++ setupmeteorproject.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+projectname=$1
+homedir="/home/vagrant";
+shareddir="$homedir/shared"
+meteordir="$shareddir/$projectname";
+mockdir="$homedir/mock/$projectname";
+
+echo "shared folder = $shareddir";
+echo "meteor folder = $meteordir";
+echo "mongo folder = $mockdir";
+
+cd $shareddir;
+meteor create $projectname;
+cd $meteordir;
+mkdir -p $mockdir;
+mv $meteordir/.meteor $mockdir;
+mkdir $meteordir/.meteor;
+
+mountcommand="sudo mount --bind $mockdir/.meteor $meteordir/.meteor";
+mountif="if ! mount | grep -q \"/home/vagrant/shared/$projectname/.meteor\"; then $mountcommand; fi"
+$mountcommand;
+
+echo "You should be able to run meteor for $projectname";
+echo "Added the following line to your .profile"
+echo "$mountif";
+echo "$mountif" >> $homedir/.bashrc 
+source $homedir/.bashrc
